7701962f9bb377a09b50f29bf5b52f88815541ef
Fabio Albertin <albertin@gmail.com>
2019-04-07 03:43:57 +0100

    Finished chapter 4

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ var app = new Vue({
     imageAlt: 'Green socks',
     inventory: 12,
     onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ],
+    sizes: [8, 8.5, 9, 9.5, 10, 10.5, 11],
     mwLink: 'http://vuejs.org'
   }
 });

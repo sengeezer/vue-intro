47b6d013f9429986310d3c4c00e8496d0905098a
Fabio Albertin <albertin@gmail.com>
2019-04-07 02:54:08 +0100

    Finished lesson 2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    desc: 'Not smelly at all'
+    desc: 'Not smelly at all',
+    image: './images/vmSocks-green-onWhite.jpg',
+    imageAlt: 'Green socks',
+    mwLink: 'http://vuejs.org'
   }
 });
